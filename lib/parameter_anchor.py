6b3a7e57c22bcbf01e3dd1b419b6fcfd0b1f2563
stsci_mdroe <stsci_mdroe@stsci.edu>
2011-05-10 18:26:26 +0000

    Add support for adding HTML anchor ids to parameters in Numpy format.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/stsci_sphinxext@2033 90a0a646-be8a-0410-bb88-9290da87bc01

---
+++ lib/parameter_anchor.py
@@ -0,0 +1,38 @@
+"""
+Adds HTML id's to the parameter names in Numpy-formatted HTML.
+"""
+
+from docutils import nodes
+
+def setup(app):
+    app.connect("doctree-resolved", add_parameter_anchors)
+
+class Visitor(nodes.GenericNodeVisitor):
+    def __init__(self, doctree):
+        nodes.GenericNodeVisitor.__init__(self, doctree)
+        self._function = ''
+
+    def default_visit(self, node):
+        pass
+
+    def visit_desc_signature(self, node):
+        if len(node['ids']):
+            self._function = node['ids'][0]
+        else:
+            self._function = ''
+
+    def visit_field(self, node):
+        if (type(node[0]) == nodes.field_name and
+            node[0].astext().strip().lower() == 'parameters'):
+            field_body = node[1]
+            for subnode in field_body:
+                parnode = subnode[0]
+                if self._function:
+                    name = self._function + '-' + parnode.astext()
+                else:
+                    name = parnode.astext()
+                parnode['ids'].append(name)
+
+def add_parameter_anchors(app, doctree, fromdocname):
+    doctree.walk(Visitor(doctree))
+

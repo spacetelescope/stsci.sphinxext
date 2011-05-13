3ce42ec41573311d86d25ac26053ee6f5fa7d5ac
stsci_mdroe <stsci_mdroe@stsci.edu>
2011-05-13 17:09:46 +0000

    Make parameter anchor use only the argument name for the id, and don't use it by default.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/stsci_sphinxext@2044 90a0a646-be8a-0410-bb88-9290da87bc01

--- lib/parameter_anchor.py
+++ lib/parameter_anchor.py
@@ -8,21 +8,11 @@ def setup(app):
     app.connect("doctree-resolved", add_parameter_anchors)
 
 class Visitor(nodes.NodeVisitor):
-    def __init__(self, doctree):
-        nodes.NodeVisitor.__init__(self, doctree)
-        self._function = ''
-
     def default_visit(self, node):
         pass
 
     def unknown_visit(self, node):
         pass
-
-    def visit_desc_signature(self, node):
-        if len(node['ids']):
-            self._function = node['ids'][0]
-        else:
-            self._function = ''
 
     def visit_field(self, node):
         if (type(node[0]) == nodes.field_name and
@@ -31,10 +21,7 @@ class Visitor(nodes.NodeVisitor):
             for subnode in field_body:
                 parnode = subnode[0]
                 if isinstance(parnode, nodes.strong):
-                    if self._function:
-                        name = self._function + '-' + parnode.astext()
-                    else:
-                        name = parnode.astext()
+                    name = parnode.astext()
                     parnode['ids'].append(name)
 
 def add_parameter_anchors(app, doctree, fromdocname):

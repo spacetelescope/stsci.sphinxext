99b1036824e2599bc553454eed9ab05b407de471
stsci_embray <stsci_embray@stsci.edu>
2011-07-01 22:57:30 +0000

    Merging the setup_refactoring branch into trunk.  This should only affect pysynphot, pywcs, stsci_python, and coords at this point
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/stsci_sphinxext@2176 90a0a646-be8a-0410-bb88-9290da87bc01

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python
-from __future__ import division # confidence high
 
 try:
-    from pytools import stsci_distutils_hack
+    from setuptools import setup
 except ImportError:
-    import stsci_distutils_hack
-stsci_distutils_hack.run(pytools_version = "3.0")
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+try:
+    from stsci.distutils.command.easier_install import easier_install
+except ImportError:
+    import os
+    import sys
+    stsci_distutils = os.path.abspath(os.path.join('..', 'distutils', 'lib'))
+    if os.path.exists(stsci_distutils) and stsci_distutils not in sys.path:
+        sys.path.append(stsci_distutils)
+    try:
+        from stsci.distutils.command.easier_install import easier_install
+        import setuptools.command.easy_install
+        setuptools.command.easy_install.easy_install = easier_install
+    except ImportError:
+        # If even this failed, we're not in an stsci_python source checkout,
+        # so there's nothing gained from using easier_install
+        from setuptools.command.easy_install import easy_install
+        easier_install = easy_install
+
+
+setup(
+    setup_requires=['d2to1', 'stsci.distutils>=0.2dev'],
+    d2to1=True,
+    use_2to3=True,
+    zip_safe=False
+)

2b0c031c6cb03a6ed20e7588864c5e0ccd288374
stsci_sienkiew <stsci_sienkiew@stsci.edu>
2012-01-25 16:20:19 +0000

    fix some problems with install
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/stsci_sphinxext@2474 90a0a646-be8a-0410-bb88-9290da87bc01

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 from __future__ import division # confidence high
 
-import stsci.tools.stsci_distutils_hack as H
-H.run()
+try :
+    import stsci.tools.stsci_distutils_hack as H
+except ImportError :
+    H=None
+
+if H :
+    H.run()
+else :
+    import distutils
+    from distutils.core import setup
+    from defsetup import setupargs, pkg
+    import glob
+
+    # copied from stsci_distutils_hack
+    l = [ ]
+    for f in setupargs['data_files'] :
+        ( dest_dir, files ) = f
+        fl = [ ]
+        for ff in files :
+            ff = distutils.util.convert_path(ff)
+            ff = glob.glob(ff)
+            fl.extend(ff)
+        dest_dir = distutils.util.convert_path(dest_dir)
+        l.append( ( dest_dir, fl ) )
+    setupargs['data_files'] = l
+
+
+    setup( name=pkg[0], packages = pkg, **setupargs )

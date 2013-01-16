b28d293e65c3806a21ad0c0a630df58a90a787f0
stsci_sienkiew <stsci_sienkiew@stsci.edu>
2013-01-16 22:51:45 +0000

    using new setup
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/stsci_sphinxext@3153 90a0a646-be8a-0410-bb88-9290da87bc01

--- setup.py
+++ setup.py
@@ -1,31 +1,17 @@
 #!/usr/bin/env python
-from __future__ import division # confidence high
 
-try :
-    import stsci.tools.stsci_distutils_hack as H
-except ImportError :
-    H=None
-
-if H :
-    H.run()
-else :
-    import distutils
-    from distutils.core import setup
-    from defsetup import setupargs, pkg
-    import glob
-
-    # copied from stsci_distutils_hack
-    l = [ ]
-    for f in setupargs['data_files'] :
-        ( dest_dir, files ) = f
-        fl = [ ]
-        for ff in files :
-            ff = distutils.util.convert_path(ff)
-            ff = glob.glob(ff)
-            fl.extend(ff)
-        dest_dir = distutils.util.convert_path(dest_dir)
-        l.append( ( dest_dir, fl ) )
-    setupargs['data_files'] = l
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
 
-    setup( name=pkg[0], packages = pkg, **setupargs )
+setup(
+    setup_requires=['d2to1', 'stsci.distutils>=0.3'],
+    d2to1=True,
+    namespace_packages=['stsci'], packages=['stsci'],
+    use_2to3=True,
+    zip_safe=False
+)
